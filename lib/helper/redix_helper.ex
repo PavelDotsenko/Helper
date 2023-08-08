34b7e98953ca43b0bc76ab1dbafd6e25ff614dab
DDoubleDee <91531619+DDoubleDee@users.noreply.github.com>
2023-08-08 15:25:24 +0600

    Update redix_helper.ex

--- lib/helper/redix_helper.ex
+++ lib/helper/redix_helper.ex
@@ -9,26 +9,66 @@ defmodule Helper.RedixHelper do
       def write(params) do
         id = Map.get(params, :id, params["id"])
         if id do
-          with {:ok, data} <- Jason.encode(params) do
-            Redix.command(:redix, ["SET", "#{@table_name}:#{id}", data])
-            |> case do
-              {:ok, _} -> params
-            end
+          Redix.command(:redix, ["SET", "#{@table_name}:#{id}", :erlang.term_to_binary()])
+          |> case do
+            {:ok, _} -> params
           end
         else
           {:error, "no id"}
         end
       end
 
+      def write(params, secondary_id) do
+        id = Map.get(params, :id, params["id"])
+        if id do
+          Redix.command(:redix, ["SET", "#{@table_name}:#{id}:#{secondary_id}", :erlang.term_to_binary()])
+          |> case do
+            {:ok, _} -> params
+          end
+        else
+          {:error, "no id"}
+        end
+      end
+
+      def time_limit(params, time) when is_map(params) do
+        id = Map.get(params, :id, params["id"])
+        if id do
+          Redix.command(:redix, ["EXPIRE", "#{@table_name}:#{id}", time])
+        else
+          {:error, "no id"}
+        end
+      end
+
+      def time_limit(id, time) do
+        Redix.command(:redix, ["EXPIRE", "#{@table_name}:#{id}", time])
+      end
+
+      def time_limit(params, secondary_id, time) when is_map(params) do
+        id = Map.get(params, :id, params["id"])
+        if id do
+          Redix.command(:redix, ["EXPIRE", "#{@table_name}:#{id}:#{secondary_id}", time])
+        else
+          {:error, "no id"}
+        end
+      end
+
+      def time_limit(id, secondary_id, time) do
+        Redix.command(:redix, ["EXPIRE", "#{@table_name}:#{id}:#{secondary_id}", time])
+      end
+
       def read(id) do
         Redix.command(:redix, ["GET", "#{@table_name}:#{id}"])
         |> case do
           {:ok, nil} -> nil
-          {:ok, data} -> Jason.decode(data, keys: :atoms)
-          |> case do
-            {:ok, data} -> data
-            {:error, _} -> nil
-          end
+          {:ok, data} -> :erlang.binary_to_term(data)
+        end
+      end
+
+      def read(id, secondary_id) do
+        Redix.command(:redix, ["GET", "#{@table_name}:#{id}:#{secondary_id}"])
+        |> case do
+          {:ok, nil} -> nil
+          {:ok, data} -> :erlang.binary_to_term(data)
         end
       end
 
@@ -43,6 +83,57 @@ defmodule Helper.RedixHelper do
         else
           {:error, "no id"}
         end
+      end
+
+      def delete(id) do
+        Redix.command(:redix, ["DEL", "#{@table_name}:#{id}"])
+        |> case do
+          {:ok, _} -> {:ok, "deleted"}
+          any -> any
+        end
+      end
+
+      def delete(params, secondary_id) when is_map(params) do
+        id = Map.get(params, :id, params["id"])
+        if id do
+          Redix.command(:redix, ["DEL", "#{@table_name}:#{id}:#{secondary_id}"])
+          |> case do
+            {:ok, _} -> {:ok, "deleted"}
+            any -> any
+          end
+        else
+          {:error, "no id"}
+        end
+      end
+
+      def delete(id, secondary_id) do
+        Redix.command(:redix, ["DEL", "#{@table_name}:#{id}:#{secondary_id}"])
+        |> case do
+          {:ok, _} -> {:ok, "deleted"}
+          any -> any
+        end
+      end
+
+      def set_get() do
+        [item | set] =
+        Redix.command(:redix, ["GET", "#{@table_name}:set"])
+        |> case do
+          {:ok, nil} -> [nil]
+          {:ok, data} -> :erlang.binary_to_term(data)
+        end
+        Redix.command(:redix, ["SET", "#{@table_name}:set", :erlang.term_to_binary(set)])
+        item
+      end
+
+      def set_add(item) do
+        set =
+        Redix.command(:redix, ["GET", "#{@table_name}:set"])
+        |> case do
+          {:ok, nil} -> []
+          {:ok, data} -> :erlang.binary_to_term(data)
+        end
+        Redix.command(:redix, ["SET", "#{@table_name}:set", :erlang.term_to_binary(set ++ [item])])
+        :ok
       end
     end
   end
